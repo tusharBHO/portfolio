d4fc99614651993cc8febc325b649965ec06ef61
Tushar Bhoyar <tbhoyar079@gmail.com>
2025-08-04 10:37:38 +0530

    fix: correct base path for Netlify

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/portfolio/', // ⚠️ if your repo name is portfolio
+  // base: '/portfolio/', // ⚠️ if your repo name is portfolio
+  base: './', // ✅ for Netlify
   plugins: [react()],
 })
